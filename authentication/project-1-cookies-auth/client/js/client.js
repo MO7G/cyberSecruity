09cda9873122a55b6d998677a6303907999bf35c
MO7A <70697952>
2023-10-16 16:10:00 -0700

    done from simple auth using cookies

---
+++ authentication/project-1-cookies-auth/client/js/client.js
@@ -0,0 +1,34 @@
+const loginMohamedBtn = document.getElementById("login-mohamed")
+const loginAliBtn = document.getElementById("login-ali")
+const loginBelalBtn = document.getElementById("login-belal")
+const adminBtn = document.getElementById("admin")
+const responsesDiv = document.getElementById("responses")
+loginMohamedBtn.addEventListener("click" , ()=>{login("mohamed")})
+loginAliBtn.addEventListener("click" , ()=>{login("ali")})
+loginBelalBtn.addEventListener("click" , ()=>{login("belal")})
+
+
+adminBtn.addEventListener("click",()=>{
+    fetch("http://localhost:3000/adminData" ,{
+        credentials: "include",
+        headers:{
+            "Content-Type" : "application/json",
+        },
+    })
+    .then(res=>res.text())
+    .then(data=>(responsesDiv.textContent = data));
+})
+
+function login(username){
+    console.log("this is the user name "  , username)
+    fetch("http://localhost:3000/login",{
+        method: "POST",
+        credentials: "include",
+        headers:{
+            "Content-Type" : "application/json",
+        },
+        body: JSON.stringify({username}),
+    })
+    .then(res=>res.text())
+    .then(data=>(responsesDiv.textContent = data));
+}
